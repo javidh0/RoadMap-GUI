9023c70076f41c545efece42ba75bedd7cc8f4ac
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2022-12-13 21:02:19 +0530

    Create Jkinter.py

---
+++ Jkinter.py
@@ -0,0 +1,14 @@
+class RoadMap(object):
+    _event = ""
+    _overal_progress = 0
+    _type = 0
+    def __init__(self, a:str, b:int, c:int = 0) -> None:
+        self._event = a
+        self._progress = b
+        self._type = c
+    def get(self):
+        pass
+
+class kinter:
+    def map_gui(obj:RoadMap):
+        pass
