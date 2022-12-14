4fe537c3b6dbe8f47e144827ec00fb8f4ab402f6
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2022-12-14 10:59:12 +0530

    Update Jkinter.py

--- Jkinter.py
+++ Jkinter.py
@@ -9,6 +9,20 @@ class RoadMap(object):
     def get(self):
         pass
 
-class kinter:
-    def map_gui(obj:RoadMap):
+class SubTask:
+    __name = None
+    __About = None
+    __progress = None
+    __type = None
+    def __init__(self, title:str = "Unknown Task", Note:str = "", progress:int = 0) -> None:
+        self.__name = title
+        self.__About = Note
+        self.__progress = progress
+    def onClick():
+        pass
+    def create():
+        pass
+    def edit():
+        pass
+    def save_in_file():
         pass
