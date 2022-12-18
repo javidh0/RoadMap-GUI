eca38e04019d6fb7b41d7f98234594ad43fb6b2d
Mohammed Javidh S <120474876+javidh0@users.noreply.github.com>
2022-12-18 20:23:53 +0530

    Update Jkinter.py

--- Jkinter.py
+++ Jkinter.py
@@ -2,10 +2,15 @@ from tkinter import *
 import pyautogui as pyg
 import pandas as pd
 pyg
+class MainWindow:
+    pass
+
 class Formate(object):
     _font = {1: "Consolas", 2:"Consolas"}
     _color= {1: "grey", 2:"grey30"}
     _size = {1: 15, 2: 15}
+
+    _Title = "RoadMap"
 
     _dFont= {1: "Consolas", 2:"Consolas"}
     _dColor= {1: "grey20", 2:"grey50"}
@@ -25,17 +30,8 @@ class SubTask(Formate):
         self.progress = progress
         self.__type = type
 
-    def __onClick(self):
-        root = Tk()
-        root.geometry("500x150")
-        root.title(self.__name)
-        lb1 = Label(root, text=self.__name, font=(self._dFont[self.__type], self._dSize[self.__type]), fg = self._dColor[self.__type])
-        lb1.pack(anchor=W)
-
-        lb2 = Label(root, text=self.__About, font=(self._dFont[2], self._dSize[2]), fg = self._dColor[2])
-        lb2.pack(anchor=W)
-
-        root.mainloop()
+    def __onClick(self, win:MainWindow):
+        return self
 
     def create(self, tk):
         self.__btn = Button(tk, text=self.__name, font=(self._font[self.__type], self._size[self.__type]), width=15)
@@ -93,6 +89,37 @@ class Data:
             temp = RoadMap(TskObj[tsk], tsk)
             toReturn.append(temp)
         return toReturn
+
+class MainWindow(Formate):
+    __rt = None
+    __sel:SubTask= None
+
+    def __init__(self) -> None:
+        self.__rt = Tk()
+        self.__rt.geometry("1000x1000")
+        self.__rt.title(self._Title)
+        width = self.__rt.winfo_screenwidth() - 50
+        height = self.__rt.winfo_screenheight() - 50
+        self.__rt.geometry('%dx%d'%(width, height))
+
+    def __clrScr(self):
+        for x in self.__rt.winfo_children():
+            x.destroy()
+    def RoadMapWindow(self):
+        self.__clrScr()
+        desFrm = LabelFrame(self.__rt)
+        desFrm.place(relx=0.01, rely=0.01, relheight=0.48, relwidth=0.38, anchor=NW)
+        toolFrm= LabelFrame(self.__rt)
+        toolFrm.place(relx=0.01, rely=0.98, relheight=0.48, relwidth=0.38, anchor=SW)
+        rdFrm = LabelFrame(self.__rt)
+        rdFrm.place(relx=0.98, rely=0.98, relheight=0.96, relwidth=0.58, anchor=SE)
+
+    def Select(self, obj:SubTask):
+        self.__sel = obj
+        
+    def Enable(self):
+        self.__rt.mainloop()
+        
 
 class Window:
     __frm:Frame = None
